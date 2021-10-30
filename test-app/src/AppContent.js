364eb51729aece861690381f8d6df31eac166908
LeeTrent <lee.trent.1@gmail.com>
2021-10-29 20:37:10 -0500

    Adds button on click event in AppContent

--- test-app/src/AppContent.js
+++ test-app/src/AppContent.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 
 export default class AppContent extends Component {
+
+    fetchList = () => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => response.json())
+        .then(json => {
+            console.log(json);
+            let posts = document.getElementById("post-list");
+            json.forEach(function(obj) {
+                let li = document.createElement("li");
+                li.appendChild(document.createTextNode(obj.title));
+                posts.appendChild(li);
+            })
+        });
+    }
+
     render() {
         return (
-            <p>
+            <div>
                 Application Content
                 <br />
-                <button class="btn btn-primary" href="#">Bootstrap Button</button>
-            </p>
-        );
-
+                <button className="btn btn-sm btn-primary mt-3" href="#" onClick={this.fetchList}>Fetch Data</button>
+                <hr/>
+                <ul id="post-list"></ul>
+            </div>
+        );    
     }
 }
