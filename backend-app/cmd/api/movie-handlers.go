ca08ddabf795b9650e89ac1b8d165020e9ed7dd5
LeeTrent <lee.trent.1@gmail.com>
2021-11-05 10:03:52 -0500

    Implements get all movies functionality

--- backend-app/cmd/api/movie-handlers.go
+++ backend-app/cmd/api/movie-handlers.go
@@ -19,27 +19,28 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 
 	app.logger.Println("Movie ID is", id)
 
-	// movie := models.Movie{
-	// 	ID:          id,
-	// 	Title:       "Some movie",
-	// 	Description: "Some description",
-	// 	Year:        2021,
-	// 	ReleaseDate: time.Date(2021, 01, 01, 01, 0, 0, 0, time.Local),
-	// 	Runtime:     100,
-	// 	Rating:      5,
-	// 	MPAARating:  "PG-13",
-	// 	CreatedAt:   time.Now(),
-	// 	UpdatedAt:   time.Now(),
-	// }
-
 	movie, err := app.models.DB.Get(id)
 	if err != nil {
 		app.errorJSON(w, err)
 		return
 	}
-	_ = app.writeJSON(w, http.StatusOK, movie, "movie")
+	err = app.writeJSON(w, http.StatusOK, movie, "movie")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 }
 
 func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
+	movies, err := app.models.DB.All()
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 
+	err = app.writeJSON(w, http.StatusOK, movies, "movies")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 }
