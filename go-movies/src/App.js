1b61c526405d3ead921d2b6e4ed7b3a7973a37a2
LeeTrent <lee.trent.1@gmail.com>
2021-11-07 10:00:49 -0600

    Working on get all genres (front-end, back-end)

--- go-movies/src/App.js
+++ go-movies/src/App.js
@@ -4,8 +4,8 @@ import {BrowserRouter as Router, Switch, Route, Link, useParams, useRouteMatch} 
 import Home from './components/Home';
 import Admin from './components/Admin';
 import Movies from './components/Movies';
-import Categories from './components/Categories';
 import OneMovie from './components/OneMovie';
+import Genres from './components/Genres';
 
 
 export default function App() {
@@ -29,7 +29,7 @@ export default function App() {
                   <Link to="/movies">Movies</Link>
                 </li>
                 <li className="list-group-item">
-                  <Link to="/by-category">Categories</Link>
+                  <Link to="/genres">Genres</Link>
                 </li>
                 <li className="list-group-item">
                   <Link to="/admin">Manage Catalog</Link>
@@ -44,17 +44,9 @@ export default function App() {
               <Route path="/movies">
                 <Movies />
               </Route>
-              <Route exact path="/by-category">
-                <CategoryPage />
+              <Route exact path="/genres">
+                <Genres />
               </Route>
-              <Route
-                exact path="/by-category/comedy"
-                render={ (props) => <Categories {...props} title={`Comedy` }/> }
-              />
-              <Route
-                exact path="/by-category/drama"
-                render={ (props) => <Categories {...props} title={`Drama` }/> }
-              />
               <Route path="/admin">
                 <Admin />
               </Route>
@@ -66,18 +58,5 @@ export default function App() {
         </div>
       </div>
     </Router>
-  );
-}
-
-function CategoryPage() {
-  let { path, url } = useRouteMatch();
-  return (
-    <div>
-      <h2>Categories</h2>
-      <ul>
-        <li><Link to={`${path}/comedy`}>Comedy</Link></li>
-        <li><Link to={`${path}/drama`}>Drama</Link></li>
-      </ul>
-    </div>
   );
 }
