fa81aa2359cf20a96fac243efe9cff6945b7bda9
LeeTrent <lee.trent.1@gmail.com>
2021-11-13 20:13:57 -0600

    Provides user with feedback (Bootstrap alert) when movie is saved to DB

--- go-movies/src/components/EditMovie.js
+++ go-movies/src/components/EditMovie.js
@@ -3,6 +3,7 @@ import './EditMovie.css';
 import Input from './form-components/Input';
 import Textarea from './form-components/Textarea';
 import Select from './form-components/Select';
+import Alert from './ui-components/Alert';
 
 export default class EditMovie extends Component {
     constructor(props) {
@@ -33,6 +34,10 @@ export default class EditMovie extends Component {
                 //"mpaa_rating"   : {errorCss: "", errorDiv: "d-none", errorMsg: ""},
                 "rating"        : {errorCss: "", errorDiv: "d-none", errorMsg: ""},
                 //"description"   : {errorCss: "", errorDiv: "d-none", errorMsg: ""}
+            },
+            alert : {
+                type: "d-none",
+                message: ""
             }
         }
 
@@ -98,7 +103,15 @@ export default class EditMovie extends Component {
         fetch('http://localhost:4000/v1/admin/editmovie', requestOptions)
         .then(response => response.json())
         .then(data => {
-            console.log(`${logSnippet} (data): `, data);
+            if (data.error) {
+                this.setState({
+                    alert: { type: "alert-danger", message: data.error.message}
+                });
+            } else {
+                this.setState({
+                    alert: { type: "alert-success", message: "Changes were successfully changed!"}
+                });            
+            }
         });
     };
 
@@ -229,6 +242,10 @@ export default class EditMovie extends Component {
             return (
                 <Fragment>
                     <h2>Add/Edit Movie</h2>
+                    <Alert 
+                        alertType={this.state.alert.type}
+                        alertMessage={this.state.alert.message}
+                    />
                     <hr />
                     <form onSubmit={this.handleSubmit}>
                         <input
@@ -299,9 +316,9 @@ export default class EditMovie extends Component {
 
                         <button className="btn btn-primary">Save</button>       
                     </form>
-                    <div className="mt-3">
+                    {/* <div className="mt-3">
                         <pre>{JSON.stringify(this.state, null, 3)}</pre>
-                    </div>
+                    </div> */}
                 </Fragment>
             )
         }
---
+++ go-movies/src/components/ui-components/Alert.js
@@ -0,0 +1,6 @@
+const Alert = (props) => {
+    return (
+        <div className={`alert ${props.alertType}`} role="alert">{props.alertMessage}</div>
+    );
+}
+export default Alert;
