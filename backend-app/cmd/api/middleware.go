96234e1c4441bf819799469af6274902895d587e
LeeTrent <lee.trent.1@gmail.com>
2021-11-17 13:44:50 -0600

    Setting up middleware to check for JWT token

--- backend-app/cmd/api/middleware.go
+++ backend-app/cmd/api/middleware.go
@@ -1,10 +1,72 @@
 package main
 
-import "net/http"
+import (
+	"errors"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/pascaldekloe/jwt"
+)
 
 func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) checkToken(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Vary", "Authorization")
+		authHeader := r.Header.Get("Authorization")
+		// if authHeader == "" {
+		// 	// could set an anonymous user
+		// }
+		headerParts := strings.Split(authHeader, " ")
+
+		if len(headerParts) != 2 {
+			app.errorJSON(w, errors.New("invlid authorization header"))
+			return
+		}
+
+		if headerParts[0] != "Bearer" {
+			app.errorJSON(w, errors.New("authorization error (no bearer token)"))
+			return
+		}
+
+		token := headerParts[1]
+		claims, err := jwt.HMACCheck([]byte(token), []byte(app.config.jwt.secret))
+		if err != nil {
+			app.errorJSON(w, errors.New("authorization error (HMAC check failed)"))
+			return
+		}
+
+		if !claims.Valid(time.Now()) {
+			app.errorJSON(w, errors.New("authorization error (JWT token has expired)"))
+			return
+		}
+
+		if !claims.AcceptAudience("mydomain.com") {
+			app.errorJSON(w, errors.New("authorization error (invalid audience)"))
+			return
+		}
+
+		if claims.Issuer != "mydomain.com" {
+			app.errorJSON(w, errors.New("authorization error (invalid issuer)"))
+			return
+		}
+
+		userID, err := strconv.ParseInt(claims.Subject, 10, 64)
+		if err != nil {
+			app.errorJSON(w, errors.New("authorization error (invalid claims.Subject)"))
+			return
+		}
+
+		log.Println("[middleware][checkToken] => (userID): ", userID)
+
 		next.ServeHTTP(w, r)
 	})
 }
