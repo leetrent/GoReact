3dd3af8cd3920450b797d76c9e01e6b902c900cc
LeeTrent <lee.trent.1@gmail.com>
2021-11-18 11:21:12 -0600

    Adds image to home page

--- go-movies/src/components/Home.js
+++ go-movies/src/components/Home.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import Ticket from "./../images/movie_tickets.jpg";
+import "./Home.css";
 
 export default class Home extends Component {
     render() {
         return (
-            <h2>You are currently on the Home page.</h2>
+            <div className="text-center">
+                <h2>Welcome to Planet Movie!</h2>
+                <hr />
+                <img src={Ticket} alt="Movie Tickets" />
+                <hr />
+                <div className="tickets"></div>
+            </div>
+
+            
+
         );
     }
 }
