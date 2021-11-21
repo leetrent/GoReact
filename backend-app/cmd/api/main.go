b71082e6fcbd61147adb41e9658acc79496617e2
LeeTrent <lee.trent.1@gmail.com>
2021-11-21 09:44:27 -0600

    Using environment variable for JWT secret

--- backend-app/cmd/api/main.go
+++ backend-app/cmd/api/main.go
@@ -45,8 +45,12 @@ func main() {
 	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
 	flag.StringVar(&cfg.env, "env", "development", "Application environment (development|production")
 	flag.StringVar(&cfg.db.dsn, "dsn", "postgres://postgres:Ca$eyPo0h@localhost/go_movies?sslmode=disable", "DB Connection String")
-	flag.StringVar(&cfg.jwt.secret, "jwt-secret", "2dce505d96a53c5768052ee90f3df2055657518dad489160df9913f66042e160", "jwt-secret")
+	//flag.StringVar(&cfg.jwt.secret, "jwt-secret", "2dce505d96a53c5768052ee90f3df2055657518dad489160df9913f66042e160", "jwt-secret")
 	flag.Parse()
+
+	// READ JWT SECRET FROM ENVIRONMENT
+	cfg.jwt.secret = os.Getenv("GO_MOVIES_JWT")
+	fmt.Println("cfg.jwt.secret", cfg.jwt.secret)
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
